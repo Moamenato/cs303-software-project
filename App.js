59c5d56445b03b6da0bf30c28fc6abbeb10eaafb
noorgx <61813444+noorgx@users.noreply.github.com>
2025-05-06 22:51:50 +0300

    update the App.js

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView, View } from "react-native";
 import Homepage from "./pages/homepage/Homepage";
 import Footer from "./components/Footer";
+import CategoriesScreen from "./pages/CategoriesScreen";
+import ItemPage from "./components/ItemPage";
+import CategoryItemsScreen from "./pages/CategoryItemsScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -12,17 +15,33 @@ const Stack = createStackNavigator();
 const AppLayout = () => {
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
-      <View style={styles.contentContainer}>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Homepage"
-            component={Homepage}
-            options={{ headerShown: false }}
-          />
-          {/* Add more screens here as needed */}
-        </Stack.Navigator>
+      <View style={styles.wrapper}>
+        <View style={styles.contentContainer}>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Homepage"
+              component={Homepage}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Categories"
+              component={CategoriesScreen}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name="CategoryItems"
+              component={CategoryItemsScreen}
+              options={{ title: "Category Items" }}
+            />
+            <Stack.Screen
+              name="ItemPage"
+              component={ItemPage}
+              options={{ title: "Item Details" }}
+            />
+          </Stack.Navigator>
+        </View>
+        <Footer />
       </View>
-      <Footer />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -42,6 +61,10 @@ const styles = StyleSheet.create({
   safeAreaContainer: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  wrapper: {
+    flex: 1,
+    flexDirection: "column",
   },
   contentContainer: {
     flex: 1,
